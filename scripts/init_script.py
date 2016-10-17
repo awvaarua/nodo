a709356537b3fafab29fa00acca9ebd53cfcefe7
aitsabesartep <ganstersb1@gmail.com>
2016-10-17 22:13:41 +0200

    dasd

--- scripts/init_script.py
+++ scripts/init_script.py
@@ -5,6 +5,5 @@ def Init(script):
 		argumentos = script.get('argumentos', [])
 		for arg in argumentos:
 				str_arg = str_arg + arg.get('valor', "") + " "
-		str_arg = str_arg + "&"
-		output = Popen(["python", "/home/pi/Scripts/temperatura.py", str_arg])
+		output = subprocess.Popen(["python", "/home/pi/Scripts/temperatura.py", str_arg])
 		return output.pid
