f53e3360ca3757a09a3770db7432f9e25f6546e2
aitsabesartep <ganstersb1@gmail.com>
2016-10-17 22:41:42 +0200

    dsa

--- scripts/update.py
+++ scripts/update.py
@@ -3,5 +3,6 @@ from uuid import getnode as get_mac
 
 def Update(oldpid, newpid):
 	url = "http://192.168.1.132:8080/nodo/"+str(get_mac())+"/script/"+str(oldpid)+"/update"
-	data = {"cambio":{"tipo":"pid","valor":newpid}}
-	r = requests.post(url, data=data)
+	req = urllib2.Request(url)
+	req.add_header('Content-Type', 'application/json')
+	response = urllib2.urlopen(req, json.dumps(data))
