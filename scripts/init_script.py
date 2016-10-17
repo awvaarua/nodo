acd1cd53d939869d69cfe5b2e8e7e066d561a15b
aitsabesartep <ganstersb1@gmail.com>
2016-10-17 21:56:02 +0200

    dsa

--- scripts/init_script.py
+++ scripts/init_script.py
@@ -5,6 +5,6 @@ def Init(script):
 		argumentos = script.get('argumentos', [])
 		for arg in argumentos:
 				str_arg = str_arg + arg.get('valor', "") + " "
-		print str_arg
-		output = Popen(["python", "temperatura.py", "&", str_arg])
+		str_arg = str_arg + " &"
+		output = Popen(["python", "temperatura.py"] + str_arg)
 		return output.pid
