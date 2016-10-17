964a65ddb9c1376e5c2b445ccd80a7b87e44d977
aitsabesartep <ganstersb1@gmail.com>
2016-10-17 23:10:43 +0200

    dsad

--- scripts/update.py
+++ scripts/update.py
@@ -3,4 +3,4 @@ from uuid import getnode as get_mac
 
 def Update(pid):
 	url = "http://192.168.1.132:8080/nodo/"+str(get_mac())+"/script/"+str(pid)+"/start"
-	json.loads(urllib2.urlopen(url).read())
+	r = urllib2.urlopen(url).read()
