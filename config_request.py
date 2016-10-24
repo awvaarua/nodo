5f2b3c6351e79a760bfe19e49400302d7d800cdc
awarua <ganstersb2@gmail.com>
2016-10-24 20:29:43 +0200

    change initial request url and remove unused init_script.py

--- config_request.py
+++ config_request.py
@@ -2,7 +2,7 @@ import urllib2, json, time, sys
 from uuid import getnode as get_mac
 with open('ip') as f:
     ip = f.readline().strip('\n')
-url = "http://"+ip+"/nodo/"+str(get_mac())+"/scripts"
+url = "http://"+ip+"/nodo/"+str(get_mac())+"/starting"
 
 while 1:
 	try:
--- init_script.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def Init(script):
-		str_arg = ""
-		argumentos = script.get('argumentos', [])
-		for arg in argumentos:
-				str_arg = str_arg + arg.get('valor', "") + " "
-		output = subprocess.Popen(["python", "/home/pi/Scripts/temperatura.py", str_arg])
-		return output.pid
